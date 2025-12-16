b6f0e86e8c808180afbf6e40d0decf149d24bf41
Emogy-reunion <mv7786986@gmail.com>
2025-12-16 06:20:56 +0300

    add: add logic that retrieves products from the database, and paginates it

--- app/routes/products.py
+++ app/routes/products.py
@@ -1,5 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.models import Products, ProductImages
+
 
 products_bp = Blueprint('products_bp', __name__)
+
+@products_bp.route('/get_product_previews', methods=['GET'])
+def get_product_previews():
+    '''
+    Retrieves products from the database
+    returns: paginated results
+    '''
+    try:
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 12))
+        category = request.args.get('category')
+
+        paginated_results = Products.query.options(selectinload(Products.images)).filter(category='networking-devices)'.paginate(page=page, per_page=per_page)
+
+        if paginated_results.items is None:
+            return jsonify({'error': 'No available products at the moment. Please try again!'}), 404
+
 
 
