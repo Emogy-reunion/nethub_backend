2754274c44ed9cef1b6df03b0eace2b877a20ffd
Emogy-reunion <mv7786986@gmail.com>
2025-12-16 10:29:45 +0300

    add: complete the logic that retrieves products preview from the database and paginates it

--- app/routes/products.py
+++ app/routes/products.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import Products, ProductImages
+from sqlalchemy.orm import selectinload
 
 
 products_bp = Blueprint('products_bp', __name__)
+
+
 
 @products_bp.route('/get_product_previews', methods=['GET'])
 def get_product_previews():
@@ -15,10 +18,35 @@ def get_product_previews():
         per_page = int(request.args.get('per_page', 12))
         category = request.args.get('category')
 
-        paginated_results = Products.query.options(selectinload(Products.images)).filter(category='networking-devices)'.paginate(page=page, per_page=per_page)
+        paginated_results = Products.query(
+            .options(selectinload(Products.images))
+            .filter_by(category=category)
+            .paginate(page=page, per_page=per_page, error_out=False)
+            )
 
-        if paginated_results.items is None:
-            return jsonify({'error': 'No available products at the moment. Please try again!'}), 404
+        if not paginated_results.items:
+            products = []
+        else:
+            products = [product.get_preview() for product in paginated_results.items]
+
+        pagination = {
+                'next': paginated_results.next_num if paginated_results.has_next else None,
+                'prev': paginated_results.prev_num if paginated_results.has_prev else None,
+                'page': paginated_results.page,
+                'pages': paginated_results.pages,
+                'total': paginated_results.total
+                }
+
+        return jsonify({
+            'pagination': pagination,
+            'products': products
+            }), 200
+
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occurred. Please try again'}), 500
+
+
+
 
 
 
