220393f5404d337d7df38f667bfd99498390d824
Emogy-reunion <mv7786986@gmail.com>
2026-01-04 12:21:39 +0300

    add: call the function that creates the initial admin" inside an application context"

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ from app import create_app
 from app.utils.create_upload_folder import create_upload_folder
 from app.routes.authentication import auth
 from app.routes.products import products_bp
+from app.utils.seed import create_initial_admin
 
 app = create_app()
 
 with app.app_context():
     create_upload_folder()
+    create_initial_admin()
 
 
 app.register_blueprint(auth, url_prefix='/api')
